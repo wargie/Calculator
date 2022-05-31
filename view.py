77537cf3700867da559c9ea39f14233096a753cd
wargie <wargie@bk.ru>
2022-05-31 06:57:59 +0200

    User Interface модуль

---
+++ view.py
@@ -0,0 +1,22 @@
+from fractions import Fraction
+import cmath
+import sys
+
+
+def get_value():
+    value = input('Введите число: ')
+    try:
+        value = Fraction(value)
+        return value
+    except ValueError:
+        value = complex(value)
+        return value
+
+
+def get_operator():
+    oper = input('Введите действие: ')
+    return oper
+
+
+def get_result(res):
+    print(f'Результат: {res}')
