a91f6219890f2a219c18848d7a2285987ca29e14
wargie <wargie@bk.ru>
2022-05-31 07:04:40 +0200

    создан модуль guid, который содержит словари, используемые в модуле controller

---
+++ guid.py
@@ -0,0 +1,8 @@
+import arithmetic.div_mod as div
+import arithmetic.mult_mod as mul
+import arithmetic.subt_mod as sub
+import arithmetic.sum_mod as sum
+
+
+dict_ar = {'/': div, '*': mul, '-': sub, '+': sum}
+dict_log = {'/': 'деление', '*': 'умножение', '-': 'вычитание', '+': 'сложение'}
